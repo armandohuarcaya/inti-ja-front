33f43d5199032f259ffe2243ec03316d127a9d7f
Cristian Huarcaya <armando_huarcaya@upeu.edu.pe>
2023-08-26 17:43:36 -0500

    Update v-partities.component.ts

--- src/app/external/intipaz/components/views/v-partities/v-partities.component.ts
+++ src/app/external/intipaz/components/views/v-partities/v-partities.component.ts
@@ -507,7 +507,7 @@ export class VPartitiesComponent implements OnInit {
       {
         grupo: 'GRUPO A',
         categoria: 'MU',
-        finalizo: 'N',
+        finalizo: 'S',
         equipeGrupo: [
           {
             logo: 'assets/imgs/logo-grupo/emaus.png',
